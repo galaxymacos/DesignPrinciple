552e280f568c27925e974dd68a7fd8249b652bf4
galaxymaxx <galaxymaxx@outlook.com>
2018-06-21 11:19:07 -0400

    + Dependency Inversion Principle

--- DependencyInversionPrinciple/Program.cs
+++ DependencyInversionPrinciple/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DependencyInversionPrinciple
 {
@@ -9,12 +11,17 @@ namespace DependencyInversionPrinciple
         Sibling
     }
 
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildOf(string name);
+    }
+
     public class Person
     {
         public string Name;
     }
 
-    public class Relationships
+    public class Relationships:IRelationshipBrowser
     {
         private List<(Person,Relationship,Person)> _relations = new List<(Person, Relationship, Person)>();
 
@@ -23,10 +30,38 @@ namespace DependencyInversionPrinciple
             _relations.Add((parent,Relationship.Parent,child));
             _relations.Add((child,Relationship.Child,parent));
         }
+
+        public List<(Person, Relationship, Person)> Relations => _relations;
+        public IEnumerable<Person> FindAllChildOf(string name)
+        {
+            foreach (var child in _relations.Where(x=>x.Item1.Name == name &&x.Item2==Relationship.Parent))
+            {
+                yield return child.Item3;
+            }
+        }
     }
     
-    internal class Program
+    internal class Research
     {
+        // In this way, the Research class is directly dependent on the relataionships class, which means that
+        // it must know how Relationships class store those data ( in this case: tuple ), In other words, the base class 
+        // once the Relationships change the way it stores data,the higher module will stop working too
+//        public Research(Relationships relationships)
+//        {
+//            var relations = relationships.Relations;
+//            foreach (var child in relations.Where(x=>x.Item1.Name == "John"&&x.Item2==Relationship.Parent))
+//            {
+//                Console.WriteLine(child.Item3.Name);
+//            }
+//        }
+
+        public Research(IRelationshipBrowser relationships)
+        {
+            foreach (var child in relationships.FindAllChildOf("John"))
+            {
+                Console.WriteLine("John has a child: "+child.Name);
+            }
+        }
         public static void Main(string[] args)
         {
             var parent = new Person {Name = "John"};
@@ -35,6 +70,8 @@ namespace DependencyInversionPrinciple
             var relationship = new Relationships();
             relationship.AddParentAndChild(parent,child1);
             relationship.AddParentAndChild(parent,child2);
+
+            new Research(relationship);
         }
     }
 }
