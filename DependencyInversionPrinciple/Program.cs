178866e9836fd52ed86bc0cee5f08b3d85bcae7c
galaxy <galaxymacos@gmail.com>
2018-06-21 10:39:18 -0400

    unfinished

---
+++ DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace DependencyInversionPrinciple
+{
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name;
+    }
+
+    public class Relationships
+    {
+        private List<(Person,Relationship,Person)> _relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            _relations.Add((parent,Relationship.Parent,child));
+            _relations.Add((child,Relationship.Child,parent));
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var parent = new Person {Name = "John"};
+            var child1 = new Person {Name = "Chris"};
+            var child2 = new Person {Name = "Mary"};
+            var relationship = new Relationships();
+            relationship.AddParentAndChild(parent,child1);
+            relationship.AddParentAndChild(parent,child2);
+        }
+    }
+}
